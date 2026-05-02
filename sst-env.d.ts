4d8a62c0fddd4efd01d546a5d0c7fefe5ecd6d80
Ally Murray <allymurray88@gmail.com>
2026-05-02 17:11:02 +0100

    Update SST email resource types

--- sst-env.d.ts
+++ sst-env.d.ts
@@ -13,6 +13,11 @@ declare module "sst" {
       "type": "sst.sst.Secret"
       "value": string
     }
+    "Email": {
+      "configSet": string
+      "sender": string
+      "type": "sst.aws.Email"
+    }
     "GoogleClientId": {
       "type": "sst.sst.Secret"
       "value": string
